94d1986bbbab9157dc75a9a0864580aac2e8fdd7
Brendan <151739865+Fluffy918@users.noreply.github.com>
2025-03-27 18:18:00 +0100

    footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <div id="footer">
+            Copyright &copy; 2024 Portfolio
+        </div>
+    )
+}
+
+export default Footer
